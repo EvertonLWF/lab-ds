aff0433e9c1188dc0c023137d0535e6e787dc992
Everton Feijo <evertonlwf@gmail.com>
2023-05-21 16:05:25 -0300

    add config viteFinal

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,5 +13,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/lab-ds/'
+    }
+
+    return config
+  }
 };
 export default config;
